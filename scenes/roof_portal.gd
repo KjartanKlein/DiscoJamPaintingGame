b700e20b857f0c9669e23d732e9b4109045e6852
Kjartan Bjarmi Árnason Klein <kjartanbklein@gmail.com>
2025-05-01 21:46:28 +0100

    added second floor and fixed teleporting door

--- scenes/roof_portal.gd
+++ scenes/roof_portal.gd
@@ -1,12 +1,13 @@
 extends Node2D
 @onready var interaction_area = $interaction
 @onready var player = %player
-
+@export var location : InteractionArea; 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction_area.interact  = Callable(self, "check_lock")
 
 func check_lock()-> void:
-	player.global_position.x -= 2000
+	player.global_position = location.global_position
+	player.global_position.x -= 200 
 	pass
--- scripts/gdscript/thedoor.gd
+++ scripts/gdscript/thedoor.gd
@@ -2,8 +2,8 @@ extends Node2D
 @onready var interaction_area = $interaction
 
 @export var unlocked = false
-@onready var player = $"../../../player"
-
+@onready var player = %player
+@export var location : InteractionArea; 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +11,8 @@ func _ready() -> void:
 
 func check_lock()-> void:
 	if unlocked:
-		player.global_position.x += 2000
-		print("teleport")
+		player.global_position = location.global_position
+		player.global_position.x += 200 
 	else:
 		print("I am locked")
 
